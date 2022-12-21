e118b6d595083fca16ce2c9cee9051ab94e40279
Halfdan Holm <halfdan@chicken.local>
2022-12-21 15:21:23 +0100

    added readme and command line arg

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import copy
 import compare
-import train
+import transformer
 import torch
+import sys
 
 
 def main():
@@ -12,23 +13,28 @@ def main():
     dropout = 0.2  # dropout probability
     ntokens = 28782  # size of vocabulary
 
-    device = train.get_device()
-    data_1, data_2, val_data, _ = train.get_dataset_split(device)
-    model_1 = train.get_model(ntokens, emsize, d_hid, nlayers, nhead, dropout)
+    device = transformer.get_device()
+    data_1, data_2, val_data, _ = transformer.get_dataset_split(device)
+    model_1 = transformer.get_model(ntokens, emsize, d_hid, nlayers, nhead, dropout)
     model_2 = copy.deepcopy(model_1)
-    model_1_trained = train.train(model_1, data_1, device, name='1')
-    model_2_trained = train.train(model_2, data_2, device, name='2')
-    #model_1_trained = torch.load('checkpoints/model_1.pt')
-    #model_2_trained = torch.load('checkpoints/model_2.pt')
-    model_1_trained.to(device)
-    loss_1 = train.evaluate(model_1_trained, val_data, device)
-    model_2_trained.to(device)
-    loss_2 = train.evaluate(model_2_trained, val_data, device)
+
+    if sys.argv[1]:
+        model_1_trained = torch.load(f'{sys.argv[2]}/model_1.pt')
+        model_2_trained = torch.load(f'{sys.argv[2]}/model_2.pt')
+        model_1_trained.to(device)
+        model_2_trained.to(device)
+    else:
+        model_1_trained = transformer.train(model_1, data_1, device, name='1')
+        model_2_trained = transformer.train(model_2, data_2, device, name='2')
+
+    loss_1 = transformer.evaluate(model_1_trained, val_data, device)
     print(f'Loss 1: {loss_1}')
+    loss_2 = transformer.evaluate(model_2_trained, val_data, device)
     print(f'Loss 2: {loss_2}')
+
     model_av = compare.average_model(model_1_trained, model_2_trained)
     model_av.to(device)
-    loss_av = train.evaluate(model_av, val_data, device)
+    loss_av = transformer.evaluate(model_av, val_data, device)
     print(f'Loss average: {loss_av}')
 
 
