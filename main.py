ee3b4827208cefef47a38ae4e5b059a9bde7a3c5
Halfdan Holm <holm.halfdan@gmail.com>
2023-01-03 00:09:43 +0000

    transformer permutations working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import copy
-import compare
+import merge
 import transformer
 import torch
 import sys
@@ -8,7 +8,7 @@ import sys
 def main():
     emsize = 64  # embedding dimension
     d_hid = 64  # dimension of the feedforward network model in nn.TransformerEncoder
-    nlayers = 2  # number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+    nlayers = 128  # number of nn.TransformerEncoderLayer in nn.TransformerEncoder
     nhead = 8  # number of heads in nn.MultiheadAttention
     dropout = 0.2  # dropout probability
     ntokens = 28782  # size of vocabulary
@@ -29,16 +29,22 @@ def main():
         model_1_trained = transformer.train(model_1, data_1, device, name='1')
         model_2_trained = transformer.train(model_2, data_2, device, name='2')
 
+    print('loaded models')
+
+    model_av = merge.average_model(model_1_trained, model_2_trained)
+    model_av.to(device)
+
+    model_aav = merge.almost_average_model(model_1_trained, model_2_trained)
+    model_aav.to(device)
+
     loss_1 = transformer.evaluate(model_1_trained, val_data, device)
     print(f'Loss 1: {loss_1}')
     loss_2 = transformer.evaluate(model_2_trained, val_data, device)
     print(f'Loss 2: {loss_2}')
-
-    model_av = compare.average_model(model_1_trained, model_2_trained)
-    model_av.to(device)
     loss_av = transformer.evaluate(model_av, val_data, device)
     print(f'Loss average: {loss_av}')
-
+    loss_aav = transformer.evaluate(model_aav, val_data, device)
+    print(f'Loss almost average: {loss_aav}')
 
 
 if __name__ == '__main__':
